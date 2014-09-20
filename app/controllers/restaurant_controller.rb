6536165cda668c325d698bd62ce7b2ddcd0f82f6
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2014-09-20 16:39:33 -0400

    This commit adds a route and controller for restaurants.  It does not include a model for restaurants yet.

---
+++ app/controllers/restaurant_controller.rb
@@ -0,0 +1,46 @@
+class CompaniesController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @reviews    = @restaurant.reviews
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+
+    if @restaurant.save
+      redirect_to @restaurant, notice: "Restaurant Successfully Added!"
+    else
+      render action: 'new'
+    end
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+
+    if @restaurant.update(restaurant_params)
+      redirect_to @restaurant, notice: "Restaurant Successfully Updated!"
+    else
+      render :action => :edit
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :city, :state, :zip_code, :category)
+      # The last two (description and category are optional)
+  end
+
+
+end
